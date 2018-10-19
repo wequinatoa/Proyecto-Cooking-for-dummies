6fb780adb8ce881a110b56a79fa9dc6be898110f
LizzieZam <43660974+LizzieZam@users.noreply.github.com>
2018-10-19 09:16:26 -0500

    Update utensillosDP.php
    Agreando funciones

--- utensillosDP.php
+++ utensillosDP.php
@@ -7,13 +7,32 @@ use Illuminate\Http\Request;
 class utencillosDP extends Controller
 {
 
-	function tipoComida($tipo){
+	function sugerenciaUtensillotipoComida($tipo){
 		
 		if($tipo!=="asiatico")
 			$tipoUtensillo='Palillos';
 		else
 			$tipoUtensillo='Cubiertos';
 		return view('utencillo',['comidaBlade'=>$tipo,'tipoUtencilloBlade'=>$tipoUtensillo]);
+	}
+	function create($descripcion,$tipo,$uso){
+		$utencillo=new Utensillo();
+		$utencillo->crearUtencillo($descripcion,$tipo,$uso);
+		$mensaje='El utensillo '.$descripcion.' fue creado exitósamente';
+		return view('utencillo',['mensajeBlade'=>$mensaje]);
+	}
+	
+	function update($id,$descripcion,$tipo,$uso){
+		$utencillo=new Utensillo();
+		$utencillo->actualizarUtencillo($id,$descripcion,$tipo,$uso);
+		$mensaje='El utensillo '.$descripcion.' fue modificado exitósamente';
+		return view('utencillo',['mensajeBlade'=>$mensaje]);
+	}
+	function delete($id){
+		$utencillo=new Utensillo();
+		$utencillo->eliminarUtencillo($id);
+		$mensaje='El utensillo fue eliminado exitósamente';
+		return view('utencillo',['mensajeBlade'=>$mensaje]);
 	}
 
     
