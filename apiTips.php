9eeeb542314984e949ed6280f4d09930708fb9bc
jeanpdcs <43657876+jeanpdcs@users.noreply.github.com>
2018-11-08 19:54:18 -0500

    Update and rename apiUsuario.php to apiTips.php

--- apiTips.php
+++ apiTips.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/Usuario/{usu}','UsuarioDP@seleccionarUsuario');
+Route::get('/Tips/{tip}','TipsDP@seleccionarTip');
