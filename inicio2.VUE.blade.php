57903af149c6626a8c692535628bb8808ba51cd9
wequinatoa <35282304+wequinatoa@users.noreply.github.com>
2018-12-06 23:05:08 -0500

    Add files via upload

---
+++ inicio2.VUE.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<htmlm lang="es">
+    <head>
+        <title>Ejemplo de Vueee</title>
+        <meta charset="UTF-8">
+        <meta name="title" content="Título de la WEB">
+        <meta name="description" content="Descripción de la WEB">
+        <link href="http://dominio.com/hoja-de-estilos.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+    <div >
+        <h1> funciona ......</h1>
+        <example-component></example-component>
+    </div>
+    </body>
+</htmlm>
